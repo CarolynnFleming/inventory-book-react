bf0a91be1c87819363cc5f74ed3d002f0512dbab
CarolynnFleming <cfcatpink@gmail.com>
2022-02-11 14:21:36 -0500

    sign in

--- src/services/fetch_utils.js
+++ src/services/fetch_utils.js
@@ -5,6 +5,11 @@ export function getuser() {
 }
 
 export async function signUp(email, password) {
-    const response = await client.auth.suignUp({ email, password });
-    return response.user;
+  const response = await client.auth.signUp({ email, password });
+  return response.user;
 }
+
+export async function signIn(email, password) {
+    const response = await client.auth.signIn({ email, password });
+    return response.user;
+  }
