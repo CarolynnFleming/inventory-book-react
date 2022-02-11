d2f11233d10ec2e798e32744d90121f6a540859b
CarolynnFleming <cfcatpink@gmail.com>
2022-02-11 16:24:04 -0500

    form

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -13,21 +13,44 @@ export default function CreatePage() {
   const [year, setYear] = useState(1);
 
   async function handleSubmit(e) {
-      e.preventDefault();
+    e.preventDefault();
 
-      await creatBook({
-          title: title,
-          author: author,
-          genre: genre,
-          sample: sample,
-          year: year
-      });
+    await creatBook({
+      title: title,
+      author: author,
+      genre: genre,
+      sample: sample,
+      year: year
+    });
 
-      history.push('/books');
+    history.push('/books');
   }
   return (
     <div className='create'>
-        <form
+      <form type="form" onSubmit={handleSubmit}>
+        <h1>Add Book</h1>
+        <label>
+                Title
+          <input required onChange={e => setTitle(e.target.value)} name='title' />
+        </label>
+        <label>
+                Author
+          <input required onChange={e => setAuthor(e.target.value)} name='author' />
+        </label>
+        <label>
+                Genre
+          <input required onChange={e => setGenre(e.target.value)} name='genre' />
+        </label>
+        <label>
+                Sample
+          <textarea required onChange={e => setSample(e.target.value)} name='sample' />
+        </label>
+        <label>
+                Year
+          <input required onChange={e => setYear(e.target.value)} name='year' />
+        </label>
+        <button>Create Book</button>
+      </form>
     </div>
   );
 }
